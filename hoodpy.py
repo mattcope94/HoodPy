0e5261109f4be7c060a4dcdc241fab9fd92fc35c
mattcope94 <mattcope94dev@gmail.com>
2020-12-29 16:25:18 -0500

    basic timeframe reporting

--- hoodpy.py
+++ hoodpy.py
@@ -4,6 +4,14 @@ import numpy as np
 import datetime as dt
 import yfinance as yf
 
-stock=input("What are we making money on today?")
+#Initial user input
+stock=input("Enter stock ticker:")
 print(stock)
 
+#Setting timeframe 
+startdate = [1,1,2020]
+start = dt.datetime(startdate[2],startdate[1],startdate[0])
+now= dt.datetime.now()
+
+df=pdr.get_data_yahoo(stock,start,now)
+print(df)
