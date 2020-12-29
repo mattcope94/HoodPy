e61fabfe42ab46f7ba64e87ca4fb109b9edb1f58
mattcope94 <mattcope94dev@gmail.com>
2020-12-29 16:16:02 -0500

    oof cleanup

--- hoodpy.py
+++ hoodpy.py
@@ -1,1 +1,9 @@
 import pandas as pd 
+from pandas_datareader import data as pdr
+import numpy as np
+import datetime as dt
+import yfinance as yf
+
+stock=input("What are we making money on today?")
+print(stock)
+
